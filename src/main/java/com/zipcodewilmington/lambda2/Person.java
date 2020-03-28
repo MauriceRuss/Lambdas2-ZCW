249789b9d0a3fdde8bea1b4715567c876224ca98
Mo Russ <mauriceruss302@gmail.com>
2020-03-28 14:39:26 -0400

    Person class created

---
+++ src/main/java/com/zipcodewilmington/lambda2/Person.java
@@ -0,0 +1,52 @@
+package com.zipcodewilmington.lambda2;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Person {
+
+
+    public enum Sex{
+        MALE, FEMALE
+    }
+
+    private String name;
+    private LocalDate birthday;
+    private Sex gender;
+    private String email;
+
+
+    public Person(String name, LocalDate birthday, Sex gender, String email) {
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void printPerson(){
+        System.out.println(name+ " " + birthday + " " + gender + " " + email);
+    }
+
+    public void printPersonList(List<Person> personList){
+        for(Person p : personList){
+            p.printPersonList(personList);
+        }
+
+    }
+}
